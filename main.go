9ebf09ddfc1b9a36d22c62d2e207f52ca04fbe29
LazyDev <devbutlazy@gmail.com>
2025-07-15 13:51:50 +0300

    feat(main): Updated parseGiftInfo to fetch and display non-hyperlinked (hidden profiled) owners instead of "N/A"
    upd(main): changed naming: extractField -> extractGiftField; parseInfo -> parseGiftInfo

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/antchfx/htmlquery"
@@ -24,10 +25,10 @@ func fetchHTML(url string) (*html.Node, error) {
 	return doc, nil
 }
 
-func extractField(doc *html.Node, field string) string {
+func extractGiftField(doc *html.Node, field string) string {
 	td := htmlquery.FindOne(doc, fmt.Sprintf(`//th[text()="%s"]/following-sibling::td`, field))
 	if td == nil {
-		return "N/A"
+		return "Unknown"
 	}
 
 	text := strings.TrimSpace(htmlquery.InnerText(td))
@@ -38,21 +39,39 @@ func extractField(doc *html.Node, field string) string {
 	return text
 }
 
-func parseInfo(doc *html.Node) map[string]string {
+func parseGiftInfo(doc *html.Node) map[string]string {
 	info := make(map[string]string)
 
-	ownerNode := htmlquery.FindOne(doc, `//th[text()="Owner"]/following-sibling::td/a`)
-	if ownerNode != nil {
-		nameNode := htmlquery.FindOne(ownerNode, "span")
-		name := strings.TrimSpace(htmlquery.InnerText(nameNode))
-		href := strings.TrimSpace(htmlquery.SelectAttr(ownerNode, "href"))
-		info["Owner"] = fmt.Sprintf("%s (%s)", name, href)
+	if ownerTd := htmlquery.FindOne(doc, `//th[text()="Owner"]/following-sibling::td`); ownerTd != nil {
+		var (
+			name string
+			href string
+		)
+
+		if a := htmlquery.FindOne(ownerTd, `a`); a != nil {
+			href = strings.TrimSpace(htmlquery.SelectAttr(a, "href"))
+
+			if span := htmlquery.FindOne(a, `span`); span != nil {
+				name = htmlquery.InnerText(span)
+			} else {
+				name = htmlquery.InnerText(a)
+			}
+		} else if span := htmlquery.FindOne(ownerTd, `span`); span != nil {
+			name = htmlquery.InnerText(span)
+		} else {
+			name = htmlquery.InnerText(ownerTd)
+		}
+
+		info["Owner"] = strings.TrimSpace(name)
+		if href != "" {
+			info["Owner"] += fmt.Sprintf(" (%s)", href)
+		}
 	} else {
-		info["Owner"] = "N/A"
+		info["Owner"] = "Unknown"
 	}
 
 	for _, field := range []string{"Model", "Backdrop", "Symbol", "Quantity"} {
-		info[field] = extractField(doc, field)
+		info[field] = extractGiftField(doc, field)
 	}
 
 	return info
@@ -68,8 +87,14 @@ func main() {
 		log.Fatalf("Error: %v", err)
 	}
 
-	info := parseInfo(doc)
+	info := parseGiftInfo(doc)
 	for key, value := range info {
-		fmt.Printf("%s: %s\n", key, value)
+		if key == "Quantity" {
+			cleaned := strings.Split(value, `/`)
+			num, _ := strconv.Atoi(strings.ReplaceAll(strings.ReplaceAll(cleaned[0], "\u00A0", ""), " ", ""))
+			fmt.Printf("%s: %v\n", key, num)
+		} else {
+			fmt.Printf("%s: %s\n", key, value)
+		}
 	}
 }
